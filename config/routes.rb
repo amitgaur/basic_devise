434e40f4f65d031c3ebd9f5b4d926051867b95db
Your Name <amitgaur@gmail.com>
2012-02-09 11:21:09 -0800

    Additional Changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ DeviseTest::Application.routes.draw do
 
 
   devise_for :users
-  resources :users, :only => :show
+  resources :users
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
